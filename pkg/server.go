eda3583a2e82ae39fe00fbe75b694c0fa60a9c3c
Odleral <odleral77@gmail.com>
2022-05-14 12:29:54 +0500

    add maps path

--- pkg/server.go
+++ pkg/server.go
@@ -35,7 +35,22 @@ func (server *Server) setupRouter() {
 
 	// Maps route
 	router.GET("/maps/workspaces", maps.GetWorkspaces)
+	router.GET("/maps/workspaces/workspace", maps.GetWorkspace)
 	router.POST("/maps/workspaces/workspace", maps.CreateWorkspace)
+	router.DELETE("/maps/workspaces/workspace", maps.DeleteWorkspace)
+	router.GET("/maps/layers", maps.GetLayers)
+	router.GET("/maps/layers/layer", maps.GetLayer)
+	router.DELETE("/maps/layers/layer", maps.DeleteLayers)
+	router.GET("/maps/datastores", maps.GetDatastores)
+	router.GET("/maps/datastores/datastore", maps.GetDatastore)
+	router.GET("/maps/datastores/datastore/exists", maps.DatastoreExists)
+	router.POST("/maps/datastores/datastore", maps.CreateDatastore)
+	router.DELETE("/maps/datastores/datastore", maps.DeleteDatastore)
+	router.GET("/maps/layergroups", maps.GetLayerGroups)
+	router.GET("/maps/layergroups/layergroup", maps.GetLayerGroup)
+	router.GET("/maps/featuretypes", maps.GetFeatureTypes)
+	router.GET("/maps/featuretypes/featuretype", maps.GetFeatureType)
+	router.DELETE("/maps/featuretypes/featuretype", maps.DeleteFeatureType)
 
 	server.router = router
 }
